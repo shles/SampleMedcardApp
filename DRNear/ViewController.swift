aade4eab53c7a1ed30e79e396026f3fbb7a12fd9
Artemiy <temitrix@gmail.com>
2018-07-31 14:01:57 -0400

    Initial Commit

---
+++ DRNear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DRNear
+//
+//  Created by Артмеий Шлесберг on 31/07/2018.
+//  Copyright © 2018 Shlesberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
