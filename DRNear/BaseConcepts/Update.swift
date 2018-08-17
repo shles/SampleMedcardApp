f2a3a0b24e86055c79c37aedf4ed8cb9ec25d5ed
Artemiy <temitrix@gmail.com>
2018-08-17 08:47:09 -0400

    moved badHabitsUdate to the separate file

--- DRNear/BaseConcepts/Update.swift
+++ DRNear/BaseConcepts/Update.swift
@@ -15,37 +15,3 @@ protocol Update: TransitionSource {
 
 }
 
-
-class MyBadHabitsUpdate: Update {
-
-    private var itemsToCommit = [Identified]()
-    private let token: Token
-    private let disposeBag = DisposeBag()
-
-    init(token: Token) {
-        self.token = token
-    }
-
-    func addItem(item: Identified) {
-        itemsToCommit += [item]
-    }
-
-    func apply() {
-        if let request = try? AuthorizedRequest(
-                path: "/eco-emc/api/my/bad-habits",
-                method: .post,
-                token: token,
-                parameters: itemsToCommit.map { $0.identification }.asParameters(),
-                encoding: ArrayEncoding()
-        ) {
-            request.make().subscribe(onNext: { _ in
-
-            }).disposed(by: disposeBag)
-        }
-
-    }
-
-    func wantsToPerform() -> Observable<Transition> {
-        return Observable.never()
-    }
-}
---
+++ DRNear/DomainConcepts/MedCard/BadHabits/BadHabitsUpdate.swift
@@ -0,0 +1,41 @@
+//
+// Created by Артмеий Шлесберг on 17/08/2018.
+// Copyright (c) 2018 Shlesberg. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class MyBadHabitsUpdate: Update {
+
+    private var itemsToCommit = [Identified]()
+    private let token: Token
+    private let disposeBag = DisposeBag()
+
+    init(token: Token) {
+        self.token = token
+    }
+
+    func addItem(item: Identified) {
+        itemsToCommit += [item]
+    }
+
+    func apply() {
+        if let request = try? AuthorizedRequest(
+                path: "/eco-emc/api/my/bad-habits",
+                method: .post,
+                token: token,
+                parameters: itemsToCommit.map { $0.identification }.asParameters(),
+                encoding: ArrayEncoding()
+        ) {
+            request.make().subscribe(onNext: { _ in
+
+            }).disposed(by: disposeBag)
+        }
+
+    }
+
+    func wantsToPerform() -> Observable<Transition> {
+        return Observable.never()
+    }
+}
