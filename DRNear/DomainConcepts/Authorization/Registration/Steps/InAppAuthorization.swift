f8c38a1685ab2fdfff2bb922308f42f16a493b2a
Igor <igor.shmakov@yahoo.com>
2018-08-27 21:48:14 +0300

    Biometric auth integration

--- DRNear/DomainConcepts/Authorization/Registration/Steps/InAppAuthorization.swift
+++ DRNear/DomainConcepts/Authorization/Registration/Steps/InAppAuthorization.swift
@@ -8,14 +8,61 @@
 
 import Foundation
 import RxSwift
+import LocalAuthentication
 
 class InAppAuthorization: Authorization {
 
-    func auth(code: String) {
-
+    private let leadingTo: () -> (UIViewController)
+    private let transitionSubject = PublishSubject<Transition>()
+    
+    init(leadingTo: @escaping () -> (UIViewController)) {
+        self.leadingTo = leadingTo
     }
 
+    let context = LAContext()
+    
+    func auth(code: String) {
+
+        // TODO: code validation
+        proceedToAccount()
+    }
+
+    func canEvaluatePolicy() -> Bool {
+        return context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+    }
+    
+    func authWithFaceId() {
+        
+        authWithBiometricID(type: .faceID)
+    }
+    
+    func authWithTouchId() {
+        
+        authWithBiometricID(type: .touchID)
+    }
+    
+    func authWithBiometricID(type: LABiometryType) {
+        
+        guard canEvaluatePolicy() else {
+            return
+        }
+        
+        let reason = "Используйте \(type == .touchID ? "Touch ID" : "Face ID") чтобы выйти в приложение"
+        
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, evaluateError in
+            if success {
+                DispatchQueue.main.async {
+                    self.proceedToAccount()
+                }
+            }
+        }
+    }
+    
+    func proceedToAccount() {
+        transitionSubject.onNext(NewWindowRootControllerTransition(leadingTo: { self.leadingTo() }))
+    }
+    
     func wantsToPerform() -> Observable<Transition> {
-        fatalError("wantsToPerform() has not been implemented")
+        return transitionSubject
     }
 }
